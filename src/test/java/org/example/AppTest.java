70ab1b0dfd48a2cb363a5b97e55bdd37c8de29e6
marinatoledo <marina.mi.toledo@hotmail.com>
2022-05-28 13:49:39 +0200

    adding test cases

--- src/test/java/org/example/AppTest.java
+++ src/test/java/org/example/AppTest.java
@@ -1,11 +1,12 @@
 package org.example;
 
+import org.apache.commons.io.FileUtils;
 import org.apache.commons.io.filefilter.WildcardFileFilter;
-import org.junit.After;
 import org.junit.Test;
 
 import java.io.File;
 import java.io.FileFilter;
+import java.io.IOException;
 import java.util.Objects;
 
 import static org.example.App.OUTPUT_SUFFIX;
@@ -14,10 +15,11 @@ import static org.junit.Assert.*;
 public class AppTest {
 
     public static final String PATH = System.getProperty("user.dir") + "/src/test/resources/";
+    public static final String PATH_SIMPLE = System.getProperty("user.dir") + "/src/test/resources/simple/";
+    public static final String PATH_COMPLEX = System.getProperty("user.dir") + "/src/test/resources/complex/";
 
-    @After
-    public void afterEachTest() {
-        File dir = new File(PATH);
+    public void deleteGeneratedResultFiles(String path) {
+        File dir = new File(path);
         FileFilter fileFilter = new WildcardFileFilter("*" + OUTPUT_SUFFIX + "*");
 
         File[] files = dir.listFiles(fileFilter);
@@ -29,16 +31,40 @@ public class AppTest {
     @Test
     public void shouldCreateOutputFile() {
         String outputFileName = PATH + "data0001_result.xml";
-
         assertFalse(new File(outputFileName).exists());
 
         App.main(new String[]{PATH, PATH});
         assertTrue(new File(outputFileName).exists());
+        deleteGeneratedResultFiles(PATH);
     }
 
     @Test
     public void shouldGenerateOutputFileName() {
         String outputFileName = App.getOutputFileName("", "inputFileName.json");
         assertEquals("inputFileName_result.json", outputFileName);
+    }
+
+    @Test
+    public void shouldCalculateSimple() throws IOException {
+        App.main(new String[]{PATH_SIMPLE, PATH_SIMPLE});
+        assertTrue(
+                FileUtils.contentEquals(
+                        new File(PATH_SIMPLE + "data0001_result.xml"),
+                        new File(PATH_SIMPLE + "result/data0001_result.xml")
+                )
+        );
+        deleteGeneratedResultFiles(PATH_SIMPLE);
+    }
+
+    @Test
+    public void shouldCalculateComplex() throws IOException {
+        App.main(new String[]{PATH_COMPLEX, PATH_COMPLEX});
+        assertTrue(
+                FileUtils.contentEquals(
+                        new File(PATH_COMPLEX + "data0002_result.xml"),
+                        new File(PATH_COMPLEX + "result/data0002_result.xml")
+                )
+        );
+        deleteGeneratedResultFiles(PATH_COMPLEX);
     }
 }
